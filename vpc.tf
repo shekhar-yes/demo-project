bcdaa6de7f4f8db6d8d12f3bc1b1a51c2ba4fe09
shekhar-yes <shekh.yes8207@gmail.com>
2026-09-24 17:27:10 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,21 +1,16 @@
-resource "google_compute_network"  "t-vpc09" {
- project = "devops-502817"
-name = "t-vpc09"
-auto_create_subnetworks = false
-  
+# VPC
+resource "google_compute_network" "t_vpc09" {
+  project                 = "devops-502817"
+  name                    = "t-vpc09"
+  auto_create_subnetworks = false
 }
 
-## subnet range 10.0.1.0/25  
-
-
-resource "google_compute_subnetwork"  "subnet2" {
-
-name = "sunbet2"
-region = "us-central1"
-ip_cidr_range = "10.0.1.0/25"
-network = google_compute_network.t-vpc09.id
-  
+# Subnet
+# CIDR: 10.0.1.0/25
+resource "google_compute_subnetwork" "subnet2" {
+  project       = "devops-502817"
+  name          = "subnet2"
+  region        = "us-central1"
+  ip_cidr_range = "10.0.1.0/25"
+  network       = google_compute_network.t_vpc09.id
 }
-
-## allow 80
-
